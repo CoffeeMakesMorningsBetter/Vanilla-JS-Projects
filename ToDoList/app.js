a396fc34b8af01e8fc2861c68bc04a554a50ec73
Mark Suzuki <markdsuzuki@gmail.com>
2018-07-18 22:38:01 -0700

    update works

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -1,37 +1,61 @@
-window.onload = function() {
+window.onload = function () {
   const todoForm = document.getElementById('todoForm')
   const list = document.getElementById('list')
+  let count = 0
+  let status = false
+  let updateID = null;
+  let allIDs = list.querySelectorAll('*[id]')
+  let char = Array.from(allIDs).map(obj => obj.id)
 
-  todoForm.addEventListener('submit', function(event) {
+  todoForm.addEventListener('submit', function (event) {
     event.preventDefault()
 
-    let removeButton = document.createElement('button')
-    removeButton.classList.add('remove')
-    removeButton.innerText = 'Delete'
+    if (char.includes(updateID) && status === true) {
 
-    let updateButton = document.createElement('button')
-    updateButton.classList.add('update')
-    updateButton.innerHTML = 'Update'
+      let update = document.getElementById('todo').value
+      document.getElementById(updateID).firstChild.innerText = update
+      todoForm.reset()
+      updateID = null
+      
+    } else {
 
-    let value = document.getElementById('todo').value
-    let el = document.createElement('span')
-    let newTodo = document.createElement('li')
-    el.innerText = value 
-    newTodo.appendChild(el)
+      let removeButton = document.createElement('button')
+      removeButton.classList.add('remove')
+      removeButton.innerText = 'Delete'
 
-    list.appendChild(newTodo)
-    newTodo.appendChild(removeButton)
-    newTodo.appendChild(updateButton)
+      let updateButton = document.createElement('button')
+      updateButton.classList.add('update')
+      updateButton.innerHTML = 'Update'
 
-    todoForm.reset()
+
+      let value = document.getElementById('todo').value
+      let el = document.createElement('span')
+      let newTodo = document.createElement('li')
+      newTodo.setAttribute('id', count)
+      el.innerText = value
+      newTodo.appendChild(el)
+      count++
+
+
+      list.appendChild(newTodo)
+      newTodo.appendChild(removeButton)
+      newTodo.appendChild(updateButton)
+
+      todoForm.reset()
+
+      status = true
+      allIDs = list.querySelectorAll('*[id]')
+      char = Array.from(allIDs).map(obj => obj.id)
+
+    }
   })
 
-  list.addEventListener('click', function(event){
-    if(event.target.className === 'remove'){
+  list.addEventListener('click', function (event) {
+    if (event.target.className === 'remove') {
       event.target.parentNode.remove()
     } else {
       document.getElementById('todo').value = event.target.previousSibling.previousSibling.innerText
-      console.log(event.target.previousSibling.previousSibling)
+      updateID = event.target.parentNode.id
     }
   })
 }
